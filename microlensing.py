5238eae12cff0f9000b3347bd3d612a5ffe4c0e1
Gal Gonen <galgonen@mail.tau.ac.il>
2024-06-25 14:52:18 +0300

    parabolicFit_bootstrapping

--- microlensing.py
+++ microlensing.py
@@ -1,92 +1,107 @@
 import sys
 from tabulate import tabulate
-
-from src.regression import parabola
+import numpy as np
+from src.regression import parabolic_fit
 from src.bootstraping import bootstrap
 from src.plotting import plot_data_and_parabola, plot_histogram_and_gaussians
 
 from src.ogle import load_event
 
 YEAR = '2019'
-ID = 'blg-0001'
+ID = 'blg-0171'
 BOOTSTRAP_SAMPLES = 10000
 MIN_DATA_POINTS = 30
 TIME_WINDOW = 20
 
+
 def points_around_peak(event):
-  return [datum for datum in event.data if abs(datum['t'] - event.metadata['Tmax'].value) < TIME_WINDOW / 2]
+    return [datum for datum in event.data if abs(datum['t'] - event.metadata['Tmax'].value) < TIME_WINDOW / 2]
+
+
+def get_tau(cut_data):
+    cut_data_time = np.array(cut_data['t'])
+    cut_data_int = np.array(cut_data['m'].value)
+    cut_data_int_err = np.array(cut_data['m'].error)
+    # WIP
+
 
 def part_1():
-  event = load_event(YEAR, ID)
+    event = load_event(YEAR, ID)
 
-  cut_data = points_around_peak(event)
+    cut_data = points_around_peak(event)
+    cut_data_time = np.array(cut_data['t'])
+    cut_data_int = np.array(cut_data['m'].value)
+    cut_data_int_err = np.array(cut_data['m'].error)
 
-  if (len(cut_data) < MIN_DATA_POINTS):
-    print(f"Data has only {len(cut_data)} points, which is less than the minimum of {MIN_DATA_POINTS}")
-    sys.exit(1)
+    if len(cut_data) < MIN_DATA_POINTS:
+        print(f"Data has only {len(cut_data)} points, which is less than the minimum of {MIN_DATA_POINTS}")
+        sys.exit(1)
 
-  predication = parabola(cut_data)
+    predication = parabolic_fit(cut_data_time, cut_data_int, cut_data_int_err)
+    print(f"Chi2 Value for parabolic fit: {predication['chi2'].value}")
+    plot_data_and_parabola(cut_data, predication)
 
-  tau_n_sigma = predication['tau'].n_sigma(event.metadata['tau'])
-  umin_n_sigma = predication['umin'].n_sigma(event.metadata['umin'])
-  tmax_n_sigma = predication['Tmax'].n_sigma(event.metadata['Tmax'])
+    #tau_n_sigma = predication['tau'].n_sigma(event.metadata['tau'])
+    umin_n_sigma = predication['umin'].n_sigma(event.metadata['umin'])
+    tmax_n_sigma = predication['Tmax'].n_sigma(event.metadata['Tmax'])
 
-  print('Finished first fit')
-  print(tabulate([
-    ['tau', predication['tau'], event.metadata['tau'], tau_n_sigma],
-    ['umin', predication['umin'], event.metadata['umin'], umin_n_sigma],
-    ['Tmax', predication['Tmax'], event.metadata['Tmax'], tmax_n_sigma]
-  ], headers=['Parameter', 'Fit', 'OGLE', 'N Sigma']))
-  
-  
-  bootstrap_predications = bootstrap(cut_data, lambda data: parabola(data), BOOTSTRAP_SAMPLES)
+    print('Finished first fit')
+    print(tabulate([
+        ['tau', predication['tau'], event.metadata['tau'], tau_n_sigma],
+        ['umin', predication['umin'], event.metadata['umin'], umin_n_sigma],
+        ['Tmax', predication['Tmax'], event.metadata['Tmax'], tmax_n_sigma]
+    ], headers=['Parameter', 'Fit', 'OGLE', 'N Sigma']))
 
-  plot_data_and_parabola(cut_data, predication)
-  plot_histogram_and_gaussians(bootstrap_predications['tau'], [predication['tau'], event.metadata['tau']], 'tau')
-  plot_histogram_and_gaussians(bootstrap_predications['umin'], [predication['umin'], event.metadata['umin']], 'umin')
-  plot_histogram_and_gaussians(bootstrap_predications['Tmax'], [predication['Tmax'], event.metadata['Tmax']], 'Tmax')
+    bootstrap_umin, bootstrap_tmax = bootstrap(cut_data_time, cut_data_int, cut_data_int_err, lambda data: parabolic_fit(data),
+                                       BOOTSTRAP_SAMPLES)
+
+    plot_data_and_parabola(cut_data, predication)
+    #plot_histogram_and_gaussians(bootstrap_predications['tau'], [predication['tau'], event.metadata['tau']], 'tau')
+    plot_histogram_and_gaussians(bootstrap_umin, [predication['umin'], event.metadata['umin']], 'umin')
+    plot_histogram_and_gaussians(bootstrap_tmax, [predication['Tmax'], event.metadata['Tmax']], 'Tmax')
+
 
 def part_2():
-  print('not implemented yet')
+    print('not implemented yet')
 
 
 if __name__ == '__main__':
-  command = sys.argv[1]
+    command = sys.argv[1]
 
-  if command == 'part1':
-    part_1()
+    if command == 'part1':
+        part_1()
 
-  elif command == 'part2':
-    part_2()
+    elif command == 'part2':
+        part_2()
 
-  elif command == 'event':
-    print(load_event(YEAR, ID))
+    elif command == 'event':
+        print(load_event(YEAR, ID))
 
-  elif command == 'max_points':
-    event = load_event(YEAR, ID)
-    points = points_around_peak(event)
-    print(f"Number of points around peak: {len(points)}")
-    tabulated_points = [[point['t'], point['m']] for point in points]
-    print(tabulate(tabulated_points, headers=['Time', 'Magnitude']))
-
-  elif command == 'good_events':
-    number_of_events = int(sys.argv[2])
-    events = []
-    for i in range(1, number_of_events + 1):
-      try:
-        print('.', end='', flush=True)
-        event = load_event(YEAR, f'blg-{str(i).zfill(4)}')
+    elif command == 'max_points':
+        event = load_event(YEAR, ID)
         points = points_around_peak(event)
-        if len(points) > MIN_DATA_POINTS:
-          events.append({ 'id': f'blg-{str(i).zfill(4)}', 'points': len(points) })
-      except:
-        print('!', end='', flush=True)
-        pass
-    print()
-    print('sorting...')
-    events = sorted(events, key=lambda event: event['points'], reverse=True)
-    print(tabulate(events))
+        print(f"Number of points around peak: {len(points)}")
+        tabulated_points = [[point['t'], point['m']] for point in points]
+        print(tabulate(tabulated_points, headers=['Time', 'Magnitude']))
 
-  else:
-    print('Invalid command')
-    sys.exit(1)
+    elif command == 'good_events':
+        number_of_events = int(sys.argv[2])
+        events = []
+        for i in range(1, number_of_events + 1):
+            try:
+                print('.', end='', flush=True)
+                event = load_event(YEAR, f'blg-{str(i).zfill(4)}')
+                points = points_around_peak(event)
+                if len(points) > MIN_DATA_POINTS:
+                    events.append({'id': f'blg-{str(i).zfill(4)}', 'points': len(points)})
+            except:
+                print('!', end='', flush=True)
+                pass
+        print()
+        print('sorting...')
+        events = sorted(events, key=lambda event: event['points'], reverse=True)
+        print(tabulate(events))
+
+    else:
+        print('Invalid command')
+        sys.exit(1)
--- src/regression.py
+++ src/regression.py
@@ -1,2 +1,18 @@
-def parabola():
-    pass
+from iminuit import Minuit
+import numpy as np
+
+
+def parabolic_fit(cut_data_time, cut_data_int, cut_data_int_err):
+    def parabola(a2, a1, a0, cut_data_time):
+        return a2 * (cut_data_time ** 2) + a1 * cut_data_time + a0
+
+    def likelihood(a2, a1, a0):
+        model = parabola(a2, a1, a0, cut_data_time)
+        return np.sum(((cut_data_int - model) / cut_data_int_err) ** 2)
+
+    initial_params = {'a2': -10, 'a1': 10, 'a0': 10}
+    fit = Minuit(likelihood, **initial_params)
+    fit.migrad()
+    t_max = -fit[1]/(2*fit[0])
+    u_min = parabola(fit[0], fit[1], fit[2], t_max)
+    return dict(a2=fit[0], a1=fit[1], a0=fit[2], chi2=fit.fval, Tmax=t_max, umin=u_min)
